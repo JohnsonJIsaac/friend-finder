3f1845b361a602cfc0b64fa836d1b77d7963e2aa
Isaac <isaac.jj2016@gmail.com>
2019-09-24 22:52:43 -0500

    some function logic, routes forged

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+let friendsArray = [
+    {
+        name:"Annie Blackburn",
+        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [5,1,4,4,5,1,2,5,4,1]     
+    },
+
+    {
+        name:"Audrey Horne",
+        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [5,5,5,5,4,5,1,2,3,4]       
+    },
+
+    {
+        name:"Shelly Johnson",
+        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [5,4,3,2,2,1,5,2,3,4]       
+    }
+];
+
+module.exports = friendsArray;
